4950c18d13beb070920def3e54d17ac14dac28c1
AceBurgundy <Samadriansabalo99@gmail.com>
2023-06-17 21:12:28 +0800

    changed secret key.

--- Engine/config.py
+++ Engine/config.py
@@ -1,8 +1,10 @@
+import os
+
 class Config:
         # Ensure templates are auto-reloaded
     TEMPLATES_AUTO_RELOAD = True
 
-    SECRET_KEY = 'Blackasdasdasdwq324535jf34f34f.'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
