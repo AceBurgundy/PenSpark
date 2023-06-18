4d9ddccb08ee06b208e32f04e41faa6e6120dcca
AceBurgundy <Samadriansabalo99@gmail.com>
2023-06-18 13:05:54 +0800

    Minor

--- Engine/__init__.py
+++ Engine/__init__.py
@@ -14,6 +14,7 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    app.secret_key = Config.SECRET_KEY
     login_manager.init_app(app)
     db.init_app(app)
 
