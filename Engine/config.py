02e9a62159a204b9338e1d14afc6da7db3a2041d
AceBurgundy <Samadriansabalo99@gmail.com>
2023-06-18 10:33:28 +0800

    minor

--- Engine/config.py
+++ Engine/config.py
@@ -1,13 +1,9 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 class Config:
         # Ensure templates are auto-reloaded
     TEMPLATES_AUTO_RELOAD = True
 
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = "02acf27fea4d3c002a063da2e71cf56e866be904fd5467670b99efa8a5d035a1"
     SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
