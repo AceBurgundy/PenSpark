04d27e0aa721d738bb9ed7a2c7c7958d100c163a
AceBurgundy <Samadriansabalo99@gmail.com>
2023-06-19 13:21:23 +0800

    Strengthened register validations.

--- Engine/user/forms.py
+++ Engine/user/forms.py
@@ -1,47 +1,87 @@
+
+import re
+from wtforms.validators import DataRequired, Length, ValidationError, Email
 from wtforms import StringField, PasswordField, EmailField
-from wtforms.validators import DataRequired, Length 
-from wtforms.validators import ValidationError
 from Engine.helpers import CheckProfanity
 from flask_wtf import FlaskForm
 from Engine.models import User
 
+def validate_username(form, field):
+    if not field.data:
+        raise ValidationError("Username cannot be empty")
+
+    user = User.query.filter_by(username=field.data).first()
+
+    if user:
+        raise ValidationError("Username already taken")
+
+def validate_email(form, field):
+    if not field.data:
+        raise ValidationError("Email cannot be empty")
+
+    user = User.query.filter_by(email=field.data).first()
+
+    if user:
+        raise ValidationError("Email already taken")
+
+def validate_password(form, field):
+    if not field.data:
+        raise ValidationError("Password cannot be empty")
+
+    # Perform additional password validation logic here
+    password = field.data
+
+    # Check for restricted words
+    restricted_words = ["password", "123456", "qwerty","where","select","update","delete",".schema","from","drop"]
+    
+    for word in restricted_words:
+        if word.lower() in password.lower():
+            raise ValidationError("Password contains a restricted word")
+
+    # Check for restricted characters
+    restricted_characters = ["!", "#", "$"]
+    for char in restricted_characters:
+        if char in password:
+            raise ValidationError("Password contains a restricted character")
+
+    # Check for at least one number and one symbol
+    if not re.search(r"\d", password):
+        raise ValidationError("Password must contain at least one number")
+
+    if not re.search(r"[!@#$%^&*()_+}{\":?></*+[;'./,]", password):
+        raise ValidationError("Password must contain at least one symbol")
+    
 class RegisterForm(FlaskForm):
-    register_username = StringField(u'Username', id="username", validators=[DataRequired(message="Add A Username"), Length(min=2,max=50), CheckProfanity(), Length(min=2,max=50)])
 
-    def validate_register_username(self, register_username):
-        
-        if not register_username.data:
-            raise ValidationError("Username cannot be empty")
-        
-        user = User.query.filter_by(username=register_username.data).first()
+    register_username = StringField(u'Username', id="username", validators=[
+        DataRequired(message="Add a username"),
+        Length(min=2, max=50),
+        validate_username,
+        CheckProfanity()
+    ])
 
-        if user:
-            raise ValidationError("Username already taken") 
-    
-    register_email = EmailField(u'Email', id="register-email", validators=[DataRequired(message="Should be a working email"), CheckProfanity(), Length(min=2,max=120)])
-    
-    def validate_register_email(self, register_email):
-        
-        if not register_email.data:
-            raise ValidationError("Email cannot be empty")        
-        
-        if "@" not in register_email.data and ".com" not in register_email.data:
-            raise ValidationError("Not an email")
-            
-        user = User.query.filter_by(email=register_email.data).first()
+    register_email = StringField(u'Email', id="register-email", validators=[
+        DataRequired(message="Should be a working email"),
+        Email(),
+        Length(min=2, max=120),
+        validate_email,
+    ])
 
-        if user:
-            raise ValidationError("Email already taken")
-            
-    register_password = PasswordField(u'Password', id="regpassword", validators=[DataRequired("Please add a password"), Length(min=2, max=40)])
+    register_password = PasswordField(u'Password', id="regpassword", validators=[
+        DataRequired("Please add a password"),
+        Length(min=2, max=40),
+        CheckProfanity(),
+        validate_password
+    ])
 
-    def validate_register_password(self, register_password):
-        if not register_password.data:
-            raise ValidationError("Password cannot be empty")    
-        
 class LoginForm(FlaskForm):
 
-    login_email = EmailField(u'Email', id="login-email", validators=[DataRequired(message="Should be a working email"), Length(min=2,max=120)])
+    login_email = EmailField(u'Email', id="login-email", validators=[
+        DataRequired(message="Should be a working email"), 
+        Length(min=2,max=120),
+        validate_email,
+        Email(),
+    ])
     
     def validate_login_email(self, login_email):
                 
@@ -56,7 +96,11 @@ class LoginForm(FlaskForm):
         if not user:
             raise ValidationError("Email not found or User not yet registered")
     
-    login_password = PasswordField(u'Password', id="logpassword", validators=[DataRequired("Please add a password"), Length(min=2, max=40)])
+    login_password = PasswordField(u'Password', id="logpassword", validators=[
+        DataRequired("Please add a password"), 
+        Length(min=2, max=40),
+        CheckProfanity()
+    ])
 
     def validate_login_password(self, login_password):
         if not login_password.data:
